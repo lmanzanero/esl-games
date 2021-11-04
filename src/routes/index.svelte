1d9a2733de788e9e97df579365682375478d251b
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-11-04 21:34:35 +0800

    added tailwin

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,144 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section style="background-color: #667eea">
+	<div class="container mx-auto px-6 text-center py-20">
+		<h2 class="mb-6 text-4xl font-bold text-center text-white">ESL Real-Time Games</h2>
+		<h3 class="my-4 text-2xl text-white">
+			A collection of games, tools, and activities to use for your classroom!
+		</h3>
+		<button
+			class="bg-white font-bold rounded-full mt-6 py-4 px-8 shadow-lg uppercase tracking-wider hover:border-red hover:text-white hover:bg-indigo-400"
+			>Get Started</button
+		>
+	</div>
+</section>
+
+<!-- Features -->
+<section class="container mx-auto px-6 p-10">
+	<h2 class="text-4xl font-bold text-center text-gray-800 mb-8">Games & Tools</h2>
+	<div class="flex items-center flex-wrap mb-20">
+		<div class="w-full md:w-1/2 pr-10">
+			<h4 class="text-3xl text-gray-800 font-bold mb-3">Score Keeper</h4>
+			<p class="text-gray-600 mb-8">
+				Their primary target will be the power generators. Prepare to open the shield. Sir, Rebel
+				ships are coming into our sector. Good. Our first catch of the day. Stand by, ion
+				control....Fire! The first transport is away.
+			</p>
+		</div>
+		<div class="w-full md:w-1/2">
+			<img class="rounded-lg" src="https://pbs.twimg.com/media/CR45LOXVEAADG5E.jpg" alt="Vortex" />
+		</div>
+	</div>
+	<div class="flex items-center flex-wrap mb-20">
+		<div class="w-full md:w-1/2">
+			<img
+				class="rounded-lg"
+				src="https://www.thesun.co.uk/wp-content/uploads/2019/06/SWJFO-EAPlay-08-1.jpg"
+				alt="use the force"
+			/>
+		</div>
+		<div class="w-full md:w-1/2 pl-10">
+			<h4 class="text-3xl text-gray-800 font-bold mb-3">Tic-Tic-Toe!</h4>
+			<p class="text-gray-600 mb-8">
+				We'll never get it out now. So certain are you. Always with you it cannot be done. Hear you
+				nothing that I say? Master, moving stones around is one thing. This is totally different.
+				No! No different!
+			</p>
+		</div>
+	</div>
+	<div class="flex items-center flex-wrap mb-20">
+		<div class="w-full md:w-1/2 pr-10">
+			<h4 class="text-3xl text-gray-800 font-bold mb-3">Connect 4</h4>
+			<p class="text-gray-600 mb-8">
+				There is no try. I can't. It's too big. Size matters not. Look at me. Judge me by my size,
+				do you? Hm? Mmmm. And well you should not. For my ally in the Force. And a powerful ally it
+				is.
+			</p>
+		</div>
+		<div class="w-full md:w-1/2">
+			<img
+				class="rounded-lg"
+				src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Ferikkain%2Ffiles%2F2018%2F01%2FRey-Luke.jpg"
+				alt="Syncing"
+			/>
+		</div>
+	</div> 
+  <div class="container mx-auto px-6 text-center py-10">
+    <button class="bg-indigo-600 text-gray-300 font-bold rounded-full mt-6 py-4 px-8 shadow-lg uppercase tracking-wider hover:border-red hover:text-white hover:bg-indigo-500">
+      View More
+    </button>
+  </div>
+</section>
+
+<!--Footer-->
+<footer class="bg-gray-100">
+	<div class="container mx-auto px-6 pt-10 pb-6">
+		<div class="flex flex-wrap">
+			<div class="w-full md:w-1/4 text-center md:text-left ">
+				<h5 class="uppercase mb-6 font-bold">Popular Games</h5>
+				<ul class="mb-4">
+					<li class="mt-2">
+						<a href="#" class="hover:underline text-gray-600 hover:text-orange-500">I'll return</a>
+					</li>
+					<li class="mt-2">
+						<a href="#" class="hover:underline text-gray-600 hover:text-orange-500">I promise</a>
+					</li>
+					<li class="mt-2">
+						<a href="#" class="hover:underline text-gray-600 hover:text-orange-500"
+							>Reckless is he</a
+						>
+					</li>
+				</ul>
+			</div>
+			<div class="w-full md:w-1/4 text-center md:text-left ">
+				<h5 class="uppercase mb-6 font-bold">Legal</h5>
+				<ul class="mb-4">
+					<li class="mt-2">
+						<a href="#" class="hover:underline text-gray-600 hover:text-orange-500"
+							>Emperor's Terms</a
+						>
+					</li>
+					<li class="mt-2">
+						<a href="#" class="hover:underline text-gray-600 hover:text-orange-500">Pulverized?</a>
+					</li>
+				</ul>
+			</div>
+			<div class="w-full md:w-1/4 text-center md:text-left ">
+				<h5 class="uppercase mb-6 font-bold">Social</h5>
+				<ul class="mb-4">
+					<li class="mt-2">
+						<a href="#" class="hover:underline text-gray-600 hover:text-orange-500">Corellia</a>
+					</li>
+					<li class="mt-2">
+						<a href="#" class="hover:underline text-gray-600 hover:text-orange-500">Bilbringi</a>
+					</li>
+					<li class="mt-2">
+						<a href="#" class="hover:underline text-gray-600 hover:text-orange-500">Fondor</a>
+					</li>
+				</ul>
+			</div>
+			<div class="w-full md:w-1/4 text-center md:text-left ">
+				<h5 class="uppercase mb-6 font-bold">Contribute</h5>
+				<ul class="mb-4">
+					<li class="mt-2">
+						<a href="#" class="hover:underline text-gray-600 hover:text-orange-500"
+							>Journey to the Emperor</a
+						>
+					</li>
+					<li class="mt-2">
+						<a href="#" class="hover:underline text-gray-600 hover:text-orange-500"
+							>Lord Vader ship approaching?</a
+						>
+					</li>
+					<li class="mt-2">
+						<a href="#" class="hover:underline text-gray-600 hover:text-orange-500">X-wing class</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
+
+<style>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
