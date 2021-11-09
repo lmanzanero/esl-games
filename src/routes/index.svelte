7d9928456c635eb6cba50084de93835e7e70bee8
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-11-10 01:24:27 +0800

    added wheel of names url

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,10 +4,13 @@
 		<h3 class="my-4 text-2xl text-white">
 			A collection of games, tools, and activities to use for your classroom!
 		</h3>
-		<a href="/whack-a-mole">
+		<a class="flex justify-center" href="https://github.com/lmanzanero/esl-games" target="_blank">
 			<button
-				class="bg-white font-bold rounded-full mt-6 py-4 px-8 shadow-lg uppercase tracking-wider hover:border-red hover:text-white hover:bg-indigo-400"
-				>Get Started</button
+				class="bg-white flex justify-center font-bold rounded-full mt-6 py-4 px-8 shadow-lg uppercase tracking-wider hover:border-red hover:text-dark hover:bg-gray-200"
+				> Contribute   
+				<svg aria-hidden="true" class="mx-2 octicon octicon-mark-github" height="24" version="1.1" viewBox="0 0 16 16" width="24"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+				</svg>
+				</button
 		></a>
 	</div>
 </section>
@@ -23,6 +26,7 @@
 				ships are coming into our sector. Good. Our first catch of the day. Stand by, ion
 				control....Fire! The first transport is away.
 			</p>
+			<a href="/score-keeper" class="text-indigo-600 hover:text-indigo-700">Demo &rarr;</a>
 		</div>
 		<div class="w-full md:w-1/2">
 			<img class="rounded-lg" src="https://pbs.twimg.com/media/CR45LOXVEAADG5E.jpg" alt="Vortex" />
@@ -37,22 +41,24 @@
 			/>
 		</div>
 		<div class="w-full md:w-1/2 pl-10">
-			<h4 class="text-3xl text-gray-800 font-bold mb-3">Tic-Tic-Toe!</h4>
+			<h4 class="text-3xl text-gray-800 font-bold mb-3">Tic-Tic-Toe</h4>
 			<p class="text-gray-600 mb-8">
 				We'll never get it out now. So certain are you. Always with you it cannot be done. Hear you
 				nothing that I say? Master, moving stones around is one thing. This is totally different.
 				No! No different!
 			</p>
+			<a href="/tic-tac-toe" class="text-indigo-600 hover:text-indigo-700">Demo &rarr;</a>
 		</div>
 	</div>
 	<div class="flex items-center flex-wrap mb-20">
 		<div class="w-full md:w-1/2 pr-10">
-			<h4 class="text-3xl text-gray-800 font-bold mb-3">Connect 4</h4>
+			<h4 class="text-3xl text-gray-800 font-bold mb-3">Whack a mole</h4>
 			<p class="text-gray-600 mb-8">
 				There is no try. I can't. It's too big. Size matters not. Look at me. Judge me by my size,
 				do you? Hm? Mmmm. And well you should not. For my ally in the Force. And a powerful ally it
 				is.
 			</p>
+			<a href="/whack-a-mole" class="text-indigo-600 hover:text-indigo-700">Demo &rarr;</a>
 		</div>
 		<div class="w-full md:w-1/2">
 			<img
@@ -62,6 +68,24 @@
 			/>
 		</div>
 	</div> 
+	<div class="flex items-center flex-wrap mb-20">
+		<div class="w-full md:w-1/2">
+			<img
+				class="rounded-lg"
+				src="https://www.thesun.co.uk/wp-content/uploads/2019/06/SWJFO-EAPlay-08-1.jpg"
+				alt="use the force"
+			/>
+		</div>
+		<div class="w-full md:w-1/2 pl-10">
+			<h4 class="text-3xl text-gray-800 font-bold mb-3">Wheel of Names</h4>
+			<p class="text-gray-600 mb-8">
+				We'll never get it out now. So certain are you. Always with you it cannot be done. Hear you
+				nothing that I say? Master, moving stones around is one thing. This is totally different.
+				No! No different!
+			</p>
+			<a href="/wheel-of-names" class="text-indigo-600 hover:text-indigo-700">Demo &rarr;</a>
+		</div>
+	</div>
   <div class="container mx-auto px-6 text-center py-10">
     <a href="/score-keeper">
 			<button class="bg-indigo-600 text-gray-300 font-bold rounded-full mt-6 py-4 px-8 shadow-lg uppercase tracking-wider hover:border-red hover:text-white hover:bg-indigo-500">
