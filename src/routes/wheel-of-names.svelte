0e1e2e8d2efc2856752ae1a4fbaeab1bad1e2207
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-11-13 00:34:10 +0800

    added wheel of names file

---
+++ src/routes/wheel-of-names.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<!-- elements go here -->
+	<title>Wheel of Names</title>
+	<meta name="description" content="A fun, free and easy to use spinner to make your class more engaging. Add names or words, and randomly choose one!">
+</svelte:head>
+<h1>Wheel of Names</h1>
