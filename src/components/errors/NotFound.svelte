82357c770da953313bb527dc30f1251695f1a76b
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-11-24 23:42:57 +0800

    added footer

--- src/components/errors/NotFound.svelte
+++ src/components/errors/NotFound.svelte
@@ -1,8 +1,15 @@
 <script>
+import Footer from "../footer/Footer.svelte";
+
   export let title;
 </script>
-<h1 class="mb-6 text-4xl font-bold text-center text-gray">Page Not Found</h1>
-<p class="text-center text-gray">{title}</p>
+<div class="container mx-auto h-screen flex flex-wrap content-center">
+  <div class="m-auto">
+    <h1 class="mb-6 text-4xl font-bold text-center text-gray animate-bounce">Page Not Found</h1>
+    <p class="text-center text-gray">{title}</p>
+  </div>
+</div>
+<Footer/>
 <style>
   @tailwind base;
   @tailwind components;
---
+++ src/components/footer/Footer.svelte
@@ -0,0 +1,76 @@
+<!--Footer-->
+<footer class="bg-gray-100">
+	<div class="container mx-auto px-6 pt-10 pb-6">
+		<div class="flex flex-wrap">
+			<div class="w-full md:w-1/4 text-center md:text-left ">
+				<h5 class="uppercase mb-6 font-bold">Popular Games</h5>
+				<ul class="mb-4">
+					<li class="mt-2">
+						<a href="/tic-tac-toe" class="hover:underline text-gray-600 hover:text-orange-500">Tic Tac Toe</a>
+					</li>
+					<li class="mt-2">
+						<a href="/whack-a-mole" class="hover:underline text-gray-600 hover:text-orange-500">Whack a mole</a>
+					</li>
+					<li class="mt-2">
+						<a href="/live-quiz" class="hover:underline text-gray-600 hover:text-orange-500"
+							>Live Quiz</a
+						>
+					</li>
+				</ul>
+			</div>
+			<div class="w-full md:w-1/4 text-center md:text-left ">
+				<h5 class="uppercase mb-6 font-bold">Popular Tools</h5>
+				<ul class="mb-4">
+					<li class="mt-2">
+						<a href="/wheel-of-names" class="hover:underline text-gray-600 hover:text-orange-500"
+							>Wheel of names</a
+						>
+					</li>
+					<li class="mt-2">
+						<a href="/score-keeper" class="hover:underline text-gray-600 hover:text-orange-500">Score Keeper</a>
+					</li>
+				</ul>
+			</div>
+			<div class="w-full md:w-1/4 text-center md:text-left ">
+				<h5 class="uppercase mb-6 font-bold">Social</h5>
+				<ul class="mb-4">
+					<li class="mt-2">
+						<a href="/" class="hover:underline text-gray-600 hover:text-orange-500">Facebook</a>
+					</li>
+					<li class="mt-2">
+						<a href="/" class="hover:underline text-gray-600 hover:text-orange-500">Instagram</a>
+					</li>
+					<li class="mt-2">
+						<a href="/" class="hover:underline text-gray-600 hover:text-orange-500">Twitter</a>
+					</li>
+				</ul>
+			</div>
+			<div class="w-full md:w-1/4 text-center md:text-left ">
+				<h5 class="uppercase mb-6 font-bold">Contribute</h5>
+				<ul class="mb-4">
+					<li class="mt-2">
+						<a href="/" class="hover:underline text-gray-600 hover:text-orange-500"
+							>Github</a
+						>
+					</li>
+					<li class="mt-2">
+						<a href="/" class="hover:underline text-gray-600 hover:text-orange-500"
+							>Suggest</a
+						>
+					</li>
+					<li class="mt-2">
+						<a href="/" class="hover:underline text-gray-600 hover:text-orange-500">Donate</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="text-center text-sm text-indigo-400 font-semibold py-3">
+			Copyright © 2021 ESL Real-Time Games
+		</div>
+	</div>	 
+</footer>
+
+<style>
+	@tailwind base;
+	@tailwind components; 
+</style>
