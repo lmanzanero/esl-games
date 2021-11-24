5c3afcac4c612afd00d8d8470f37c615e7f2fc50
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-11-24 23:48:42 +0800

    added home page link

--- src/components/errors/NotFound.svelte
+++ src/components/errors/NotFound.svelte
@@ -7,6 +7,7 @@ import Footer from "../footer/Footer.svelte";
   <div class="m-auto">
     <h1 class="mb-6 text-4xl font-bold text-center text-gray animate-bounce">Page Not Found</h1>
     <p class="text-center text-gray">{title}</p>
+    <p class="text-center text-gray">Go to <a class="text-blue-400 hover:text-blue-700" href="/">Home Page</a></p>
   </div>
 </div>
 <Footer/>
