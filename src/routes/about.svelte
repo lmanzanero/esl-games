8387e476e3dd33f30c9a8e056bde05bd451a89e0
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-11-27 16:05:09 +0800

    add seo and og meta data

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,1 +1,20 @@
+<svelte:head>
+	<!-- elements go here -->
+	<title>About</title>
+	<meta name="description" content="ESL Real Time Games was made by teachers for teachers! Enjoy this collection of ESL online games in your class!">
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://esl-games.vercel.app/about">
+	<meta property="og:title" content="ESL Real-Time Games -  Our Story!">
+	<meta property="og:description" content="ESL Real Time Games was made by teachers for teachers! Enjoy this collection of ESL online games in your class!!">
+	<meta property="og:image" content="https://esl-games.vercel.app/esl-social.jpg">
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image">
+	<meta property="twitter:url" content="https://esl-games.vercel.app/">
+	<meta property="twitter:title" content="ESL Real-Time Games -  Our Story!">
+	<meta property="twitter:description" content="ESL Real Time Games was made by teachers for teachers! Enjoy this collection of ESL online games in your class!">
+	<meta property="twitter:image" content="https://esl-games.vercel.app/esl-social.jpg">
+</svelte:head>
 <h1>About</h1>
+<p>ESL Real Time Games was made by teachers for teachers! Enjoy this collection of ESL online games in your class!</p>
