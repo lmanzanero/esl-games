e647b7af3a5b596aa8a3169e309944844b07a293
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-12-17 21:43:13 +0800

    fixed state issues

--- src/routes/whack-a-mole.svelte
+++ src/routes/whack-a-mole.svelte
@@ -21,7 +21,10 @@
 			score = 0;
 			peep();
 			console.log('Starting Game');
-			setTimeout(() => (timeUp = true), 10000);
+			setTimeout(() => {
+				timeUp = true;
+				isListening = false;
+			}, 10000);
 		}
 
 		document.querySelector('.start').addEventListener('click', startGame);
@@ -96,7 +99,7 @@
 		on:click={() => (isListening = !isListening)}
 	>
 		<svg
-			class={`animate-spin -ml-1 mr-3 h-5 w-5 text-white ${isListening ? 'hidden' : ''}`}
+			class={`animate-spin -ml-1 mr-3 h-5 w-5 text-white ${!isListening ? 'hidden' : ''}`}
 			xmlns="http://www.w3.org/2000/svg"
 			fill="none"
 			viewBox="0 0 24 24"
@@ -113,7 +116,7 @@
 				d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
 			/></svg
 		>
-		{isListening ? 'Start Game' : 'Listening ...'}
+		{!isListening ? 'Start Game' : 'Listening ...'}
 	</button>
 	<p class="text-center p-4 m-auto text-white text-base font-semibold">
 		You Said: <span class="text-green-700">Hello</span>
