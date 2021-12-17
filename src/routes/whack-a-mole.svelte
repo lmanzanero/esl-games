827320d064b5726ff752e8e0871e83ea618a8730
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-12-17 13:21:33 +0800

    added loading indicator to start button

--- src/routes/whack-a-mole.svelte
+++ src/routes/whack-a-mole.svelte
@@ -3,6 +3,7 @@
 	let lastHole;
 	let timeUp = false;
 	let score = 10;
+	let isListening = false;
 
 	onMount(() => {
 		const holes = document.querySelectorAll('.hole');
@@ -90,9 +91,33 @@
 		</div>
 	</div>
 	<button
-		class="bg-indigo-600 start flex justify-center text-white text-base font-semibold m-auto px-6 py-2 rounded-lg"
-		>Start Game</button
+		type="button"
+		class="flex start justify-center justify-center m-auto px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-purple-600 hover:bg-rose-500 focus:border-rose-700 active:bg-rose-700 transition ease-in-out duration-150"
+		on:click={() => (isListening = !isListening)}
 	>
+		<svg
+			class={`animate-spin -ml-1 mr-3 h-5 w-5 text-white ${isListening ? 'hidden' : ''}`}
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			><circle
+				class="opacity-25"
+				cx="12"
+				cy="12"
+				r="10"
+				stroke="currentColor"
+				stroke-width="4"
+			/><path
+				class="opacity-75"
+				fill="currentColor"
+				d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+			/></svg
+		>
+		{isListening ? 'Start Game' : 'Listening ...'}
+	</button>
+	<p class="text-center p-4 m-auto text-white text-base font-semibold">
+		You Said: <span class="text-green-700">Hello</span>
+	</p>
 </div>
 
 <style>
