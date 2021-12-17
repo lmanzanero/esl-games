01cc1e02c928f0717f05109af99e7191a457356e
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-12-18 00:53:59 +0800

    added continue speech recongition to game

--- src/routes/whack-a-mole.svelte
+++ src/routes/whack-a-mole.svelte
@@ -8,12 +8,14 @@
 	let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 	const recognition = new SpeechRecognition();
 	recognition.interimResults = true;
+	recognition.continuous = true;
 	recognition.addEventListener('result', (e) => {
 		const transcript = Array.from(e.results)
 			.map((result) => result[0])
 			.map((result) => result.transcript)
 			.join('');
 		speech = transcript;
+		console.log(transcript);
 	});
 
 	onMount(() => {
