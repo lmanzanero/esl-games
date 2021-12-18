cb07c88671a8afd62b0173fd0bfd8656b8e8f8b7
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-12-18 18:03:58 +0800

    added og meta tags

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -1,6 +1,7 @@
 <script context="module">
 	export async function load({ page, fetch }) {
 		const post = await fetch(`${page.path}.json`).then((res) => res.json());
+		const pagePath = page.path;
 		return {
 			props: {
 				post
@@ -14,10 +15,30 @@
 	import Header from '../../components/header/Header.svelte';
 
 	export let post;
+	export let pagePath;
 </script>
 
 <svelte:head>
 	<title>{post.metaData.title}</title>
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://esl-games.vercel.app/{pagePath}" />
+	<meta property="og:title" content={post.metaData.title} />
+	<meta
+		property="og:description"
+		content="A collection of real time online games, tools, and activities to use in your classroom or to practice to learn English!"
+	/>
+	<meta property="og:image" content="https://esl-games.vercel.app/esl-social.jpg" />
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://esl-games.vercel.app/{pagePath}" />
+	<meta property="twitter:title" content={post.metaData.title} />
+	<meta
+		property="twitter:description"
+		content="A collection of real time online games, tools, and activities to use in your classroom or to practice to learn English!"
+	/>
+	<meta property="twitter:image" content="https://esl-games.vercel.app/esl-social.jpg" />
 </svelte:head>
 <Header />
 <article>
