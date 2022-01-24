cf1c2f09ca90788de85467852cba274c4b8181fb
Luis Manzanero <manzanero.luis1995@gmail.com>
2022-01-25 02:16:50 +0800

    added sign up alert on if user does not have account

--- src/components/score-keeper/ScoreHeader.svelte
+++ src/components/score-keeper/ScoreHeader.svelte
@@ -66,6 +66,31 @@
 	}
 </script>
 
+{#if !supabase.auth.user()}
+	<div class="bg-indigo-900 text-center py-4 lg:px-4">
+		<div
+			class="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
+			role="alert"
+		>
+			<span class="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3"
+				>New</span
+			>
+			<span class="font-semibold mr-2 text-left flex-auto"
+				>Create an account and save your data!</span
+			>
+			<a href="/login">
+				<svg
+					class="fill-current opacity-75 h-4 w-4"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 20 20"
+					><path
+						d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"
+					/></svg
+				></a
+			>
+		</div>
+	</div>
+{/if}
 <div class="bg-white fixed w-full overflow-hidden z-10 h-32 m-auto shadow">
 	<h1 class="text-lg leading-6 font-medium text-black text-center mt-3 mb-3">Score Keeper</h1>
 	<div class="flex justify-center">
