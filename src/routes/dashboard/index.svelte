3c9b512fa3f49bf419d97a4a6abf1c84f454e758
Luis Manzanero <manzanero.luis1995@gmail.com>
2022-05-19 22:25:55 +0800

    updated layout with sidebar

--- src/routes/dashboard/index.svelte
+++ src/routes/dashboard/index.svelte
@@ -33,65 +33,29 @@
 	});
 </script>
 
-<div class="bg-white flex flex-nowrap p-3 gap-2">
-	<div class="tabs flex flex-col mt-20 w-42 h-full rounded shadow">
+<!-- Slot -->
+<div class="bg-white">
+	<div class="flex justify-between m-2">
+		<h3 class="text-4xl font-bold">Dashboard</h3>
 		<button
-			data-target="panel-1"
-			class="tab active text-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none {selectedTab ===
-			'panel-1'
-				? 'text-blue-500 border-b-2 font-medium border-blue-500'
-				: ''}"
+			class="bg-indigo-600 hover:bg-indigo-700 hover:text-gray-300 group flex items-center rounded-md text-white text-sm font-medium px-4 py-2 mx-2"
 		>
-			Collections
-		</button><button
-			data-target="panel-2"
-			class="tab ext-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none {selectedTab ===
-			'panel-2'
-				? 'text-blue-500 border-b-2 font-medium border-blue-500'
-				: ''}"
-		>
-			Classes
-		</button><button
-			data-target="panel-3"
-			class="tab text-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none {selectedTab ===
-			'panel-3'
-				? 'text-blue-500 border-b-2 font-medium border-blue-500'
-				: ''}"
-		>
-			Games
+			<svg
+				class="group-hover:text-light-blue-600 text-light-blue-500 mr-2"
+				width="12"
+				height="20"
+				fill="currentColor"
+			>
+				<path
+					fill-rule="evenodd"
+					clip-rule="evenodd"
+					d="M6 5a1 1 0 011 1v3h3a1 1 0 110 2H7v3a1 1 0 11-2 0v-3H2a1 1 0 110-2h3V6a1 1 0 011-1z"
+				/>
+			</svg>
+			New
 		</button>
 	</div>
-	<div id="panels" class="w-full bg-white">
-		<div class="panel-1 tab-content active py-5">
-			<div class="flex justify-between m-2">
-				<h3 class="text-4xl font-bold">Collections</h3>
-				<button
-					class="bg-indigo-600 hover:bg-indigo-700 hover:text-gray-300 group flex items-center rounded-md text-white text-sm font-medium px-4 py-2 mx-2"
-				>
-					<svg
-						class="group-hover:text-light-blue-600 text-light-blue-500 mr-2"
-						width="12"
-						height="20"
-						fill="currentColor"
-					>
-						<path
-							fill-rule="evenodd"
-							clip-rule="evenodd"
-							d="M6 5a1 1 0 011 1v3h3a1 1 0 110 2H7v3a1 1 0 11-2 0v-3H2a1 1 0 110-2h3V6a1 1 0 011-1z"
-						/>
-					</svg>
-					New
-				</button>
-			</div>
-			<div class="bg-purple-300 rounded shadow p-2">Hello Collections Box</div>
-		</div>
-		<div class="panel-2 tab-content py-5 bg-purple-300 rounded shadow">
-			<h3 class="text-4xl font-bold">Classes</h3>
-		</div>
-		<div class="panel-3 tab-content py-5 bg-purple-300 rounded shadow">
-			<h3 class="text-4xl font-bold">Games</h3>
-		</div>
-	</div>
+	<div class="bg-purple-300 rounded shadow p-2">Hello Collections Box</div>
 </div>
 
 <style>
