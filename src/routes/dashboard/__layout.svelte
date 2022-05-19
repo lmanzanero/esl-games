acb68a34adc599baec9241c7647a0f301684dd3b
Luis Manzanero <manzanero.luis1995@gmail.com>
2022-05-19 22:33:27 +0800

    updated layout with dashbaord header

--- src/routes/dashboard/__layout.svelte
+++ src/routes/dashboard/__layout.svelte
@@ -3,13 +3,13 @@
 	import { supabase } from '$lib/dbConfig';
 	import SideBar from '../../components/sidebar/SideBar.svelte';
 	import { onMount } from 'svelte';
-	import Header from '../../components/header/Header.svelte';
+	import DashboardHeader from '../../components/header/DashboardHeader.svelte';
 	onMount(() => {
 		if (!supabase.auth.session()) goto('/login');
 	});
 </script>
 
-<Header />
+<DashboardHeader />
 <div class="flex w-full">
 	<SideBar />
 	<div class="flex-1 px-5">
