b2baa5d52f38abbd540a2b56239991ece2f70e52
Luis Manzanero <manzanero.luis1995@gmail.com>
2022-05-21 01:02:14 +0800

    used switch component to toggle public and private

--- src/routes/dashboard/create.svelte
+++ src/routes/dashboard/create.svelte
@@ -1,1 +1,14 @@
-<h1>Create</h1>
+<script>
+	import Switch from '../../components/common/Switch.svelte';
+	let isPrivate = false;
+</script>
+
+<div class="bg-white">
+	<div class="flex justify-between bg-purple-300 rounded shadow p-3">
+		<h3 class="text-4xl font-bold">Create a Collection</h3>
+		<div class="flex justify-center items-center gap-2">
+			<Switch bind:checked={isPrivate} />
+			<p class="text-gray-600 font-bold">{isPrivate ? 'Private' : 'Public'}</p>
+		</div>
+	</div>
+</div>
