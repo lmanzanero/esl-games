d51449d53b559e718f7451f2dd4323efb97455e0
Luis Manzanero <manzanero.luis1995@gmail.com>
2022-05-21 14:56:55 +0800

    added question components to page

---
+++ src/components/questions/Question.svelte
@@ -0,0 +1,58 @@
+<div class="w-full h-28 my-2 shadow-md flex items-center bg-purple-300 rounded">
+	<img
+		class="w-30 h-full object-cover"
+		src="https://media.istockphoto.com/vectors/image-preview-icon-picture-placeholder-for-website-or-uiux-design-vector-id1222357475?k=20&m=1222357475&s=170667a&w=0&h=YGycIDbBRAWkZaSvdyUFvotdGfnKhkutJhMOZtIoUKY="
+		alt="Question"
+	/>
+	<p class="w-full truncate text-xl text-gray-600 pl-4">Where is pluto?</p>
+	<div class="flex flex-col pr-2">
+		<button class="h-1/3 hover:shadow p-1 text-purple-500 hover:text-purple-700 rounded-lg">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				stroke-width="2"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+				/>
+			</svg>
+		</button>
+		<button class="h-1/3 hover:shadow p-1 text-purple-500 hover:text-purple-700 rounded-lg">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				stroke-width="2"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+				/>
+			</svg>
+		</button>
+		<button class="h-1/3 hover:shadow p-1 text-purple-500 hover:text-purple-700 rounded-lg">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				stroke-width="2"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+				/>
+			</svg>
+		</button>
+	</div>
+</div>
