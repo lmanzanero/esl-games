41539b7029782cb54ebeaaf58ad806265034f9d1
Luis Manzanero <manzanero.luis1995@gmail.com>
2022-05-21 18:31:32 +0800

    created spreed sheet uploader component

---
+++ src/components/questions/SpreedSheetQUploader.svelte
@@ -0,0 +1,30 @@
+<div
+	class="flex flex-col items-center justify-center gap-5 bg-purple-300 p-3 shadow border-2 border-dotted border-gray-600 rounded"
+>
+	<h3 class="text-2xl font-bold text-gray-600">Spreedsheet Upload</h3>
+	<ol class="list-decimal leading-8 text-gray-600 ">
+		<li>Copy or Download our template</li>
+		<li>Fill it out and export as CSV</li>
+		<li>Upload below</li>
+	</ol>
+	<label
+		class="w-40 flex flex-row p-2 gap-3 items-center justify-center bg-white text-blue rounded-lg shadow-lg text-xs border border-blue cursor-pointer hover:bg-blue hover:text-gray-600"
+	>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="h-9 w-9 text-purple-400"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+			stroke-width="2"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M3 10h18M3 14h18m-9-4v8m-7 0h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
+			/>
+		</svg>
+		<span class="text-sm text-purple-400">Upload CSV</span>
+		<input type="file" class="hidden" />
+	</label>
+</div>
