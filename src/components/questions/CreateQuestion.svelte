147688b1c0d62230a5f776caede1aa334ca07206
Luis Manzanero <manzanero.luis1995@gmail.com>
2022-05-21 17:35:16 +0800

    updated close icons to use CloseBadge component

--- src/components/questions/CreateQuestion.svelte
+++ src/components/questions/CreateQuestion.svelte
@@ -1,7 +1,12 @@
+<script>
+	import CloseBadge from '../badges/CloseBadge.svelte';
+	import CreateAnswer from './CreateAnswer.svelte';
+</script>
+
 <div class="flex flex-col">
 	<h3 class="p-2 text-gray-600">Question</h3>
 	<div class="flex gap-2">
-		<button class="p-2 border rounded text-purple-400 hover:shadow hover:text-purple-600">
+		<button class="relative p-2 border rounded text-purple-400 hover:shadow hover:text-purple-600">
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				class="h-6 w-6"
@@ -16,8 +21,9 @@
 					d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
 				/>
 			</svg>
+			<CloseBadge />
 		</button>
-		<button class="p-2 border rounded text-purple-400 hover:shadow hover:text-purple-600"
+		<button class="relative p-2 border rounded text-purple-400 hover:shadow hover:text-purple-600"
 			><svg
 				xmlns="http://www.w3.org/2000/svg"
 				class="h-6 w-6"
@@ -31,56 +37,18 @@
 					stroke-linejoin="round"
 					d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"
 				/>
-			</svg></button
-		>
+			</svg>
+			<CloseBadge />
+		</button>
 		<input
 			type="text"
 			class="bg-white border border-purple-500 text-gray-600 placeholder-gray-700 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full py-3 px-2"
 			placeholder="Write Question..."
 		/>
 	</div>
-	<div class="flex flex-col">
+	<div class="flex flex-col gap-3">
 		<h3 class="p-2 text-gray-600">Answers</h3>
-		<div class="flex gap-2">
-			<button class="p-2 border rounded text-green-400 hover:shadow hover:text-green-500">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-6 w-6"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-					stroke-width="2"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-					/>
-				</svg>
-			</button>
-			<input
-				type="text"
-				class="bg-white border border-purple-500 text-gray-600 placeholder-gray-700 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full py-3 px-2"
-				placeholder="Write Answer"
-			/>
-			<button
-				class="relative p-2 border rounded text-purple-400 hover:shadow hover:text-purple-600"
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-6 w-6"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-					stroke-width="2"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-					/>
-				</svg>
-			</button>
-		</div>
+		<CreateAnswer isCorrect={true} />
+		<CreateAnswer isCorrect={false} />
 	</div>
 </div>
